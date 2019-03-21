de13e406facf0b1b2867817979d2c9904c936c35
Noé Colin <noe_colin@yahoo.fr>
2019-03-21 11:41:35 +0100

    51 done

---
+++ TodoListIos/TodoListIos/Modeles/Checklist.swift
@@ -0,0 +1,19 @@
+//
+//  Checklist.swift
+//  TodoListIos
+//
+//  Created by lpiem on 21/03/2019.
+//  Copyright © 2019 lpiem. All rights reserved.
+//
+
+import UIKit
+
+class Checklist {
+    var name : String
+    var items : [ChecklistItem]
+    
+    init(text:String, itemsList: [ChecklistItem] = []){
+        self.name = text
+        self.items = itemsList
+    }
+}
