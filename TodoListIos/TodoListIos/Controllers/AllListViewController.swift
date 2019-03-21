d155a454c59b215434124bad24a47025a9b99c87
Noé Colin <noe_colin@yahoo.fr>
2019-03-21 09:20:09 +0100

    48 done

---
+++ TodoListIos/TodoListIos/Controllers/AllListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AllListViewController.swift
+//  TodoListIos
+//
+//  Created by lpiem on 21/03/2019.
+//  Copyright © 2019 lpiem. All rights reserved.
+//
+
+import UIKit
+
+class AllListViewController: UITableViewController {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+        // Configure the view for the selected state
+    
+
+}
